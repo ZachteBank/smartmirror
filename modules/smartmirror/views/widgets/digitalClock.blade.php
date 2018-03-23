5a0756ef9aa58c29fd49a75be1caadc1ffa9d2a8
jensv <jensverstappen@hotmail.com>
2018-03-23 13:59:46 +0100

    add working digital clock

--- modules/smartmirror/views/widgets/digitalClock.blade.php
+++ modules/smartmirror/views/widgets/digitalClock.blade.php
@@ -11,3 +11,27 @@
         <span id="timeDigital">15:50</span>
     </div>
 </div>
+
+@push("afterScripts")
+
+<script>
+    $(document).ready(function(){
+        startTime();
+    });
+
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        m = checkTime(m);
+        document.getElementById('timeDigital').innerHTML =
+            h + ":" + m;
+        var t = setTimeout(startTime, 10000);
+    }
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+</script>
+
+    @endpush
