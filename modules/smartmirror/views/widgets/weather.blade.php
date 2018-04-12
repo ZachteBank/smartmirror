82a7c0d3f294bfd76c40a9e48cc404472ebe972c
bramk <bramkempen@gmail.com>
2018-04-12 11:24:51 +0200

    update the weather every minute

--- modules/smartmirror/views/widgets/weather.blade.php
+++ modules/smartmirror/views/widgets/weather.blade.php
@@ -27,10 +27,18 @@
 
 
     <script>
-        $.getJSON('http://api.openweathermap.org/data/2.5/weather?q={{$location}}&appid=a08eb18e2263126de2d45502c41e224b&units=metric', function(data) {
-            console.log(data);
-            setWeather(data);
+        $(document).ready(function () {
+           refreshWeather();
         });
+
+        function refreshWeather() {
+            $.getJSON('http://api.openweathermap.org/data/2.5/weather?q={{$location}}&appid=a08eb18e2263126de2d45502c41e224b&units=metric', function(data) {
+                console.log(data);
+                setWeather(data);
+            });
+
+            setInterval(refreshWeather, 60000);
+        }
 
         function setWeather(data){
             setTemp(data.main.temp);
