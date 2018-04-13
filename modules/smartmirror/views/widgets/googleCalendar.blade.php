8f53335b99f796eb27dbb297470cdbb7430245da
jensv <jensverstappen@hotmail.com>
2018-04-13 21:37:31 +0200

    add google calendar, only today and tomorrow, calendar not working

--- modules/smartmirror/views/widgets/googleCalendar.blade.php
+++ modules/smartmirror/views/widgets/googleCalendar.blade.php
@@ -20,25 +20,42 @@
     <script>
         $.getJSON('{{route("google.calendar.all")}}', function(data) {
             console.log(data);
-            writeCalendar(data);
+            writeCalendarToday(data);
+            writeCalendarTomorrow(data);
         });
 
-        function writeCalendar(data) {
+        function writeCalendarToday(data) {
             console.log(data);
+            var today = new Date();
+
+            $("#calendar ul").append("<li>"+"Vandaag:"+"</li>");
             for (var obj of data.items) {
-                $("#calendar ul").append("<li>"+obj.summary + " " + convertTime(obj.start.dateTime) + "-" + convertTime(obj.end.dateTime)+"</li>");
+                if(obj.date == today) {
+                    $("#calendar ul").append("<li>" + obj.summary + " " + convertTime(obj.start.dateTime) + "-" + convertTime(obj.end.dateTime) + "</li>");
+                }
+            }
+        }
+
+        function writeCalendarTomorrow(data) {
+            console.log(data);
+            var day = new Date('Apr 30, 2000');
+
+            var tomorrow = new Date(day);
+            tomorrow.setDate(day.getDate()+1);
+
+            $("#calendar ul").append("<li>"+"Morgen:"+"</li>");
+            for (var obj of data.items) {
+                if(obj.date == tomorrow) {
+                    $("#calendar ul").append("<li>" + obj.summary + " " + convertTime(obj.start.dateTime) + "-" + convertTime(obj.end.dateTime) + "</li>");
+                }
             }
         }
 
         function convertTime(time) {
             var date = new Date(time);
             console.log(date);
-            var hh = date.getHours();
-            var mm = date.getMinutes();
-            if (mm < 10)
-            {
-
-            }
+            var hh = dateToDouble(date.getHours());
+            var mm = dateToDouble(date.getMinutes());
             return hh + ':' + mm;
         }
     </script>
