a5604eb5e1831ff0fd5698a6887017669c023c60
bramk <bramkempen@gmail.com>
2018-04-20 14:11:13 +0200

    remove border

--- modules/smartmirror/views/widgets/weather.blade.php
+++ modules/smartmirror/views/widgets/weather.blade.php
@@ -11,7 +11,7 @@
     <div id="weather" class="x_content text-center">
         <div class="weatherdata hidden">
             <span id="weatherCurrent"><span id="weatherCurrentTemp"></span>&deg;</span>
-            <div class="ln_solid"></div>
+            
             <span id="weatherIcon"><i class="fa fa-cloud"></i><i class=""></i></span>
         </div>
     </div>
